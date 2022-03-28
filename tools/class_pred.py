07c5d2199e50065acbd3bd4a1f3bcb4af40ff741
cybercore <ntqvinh21@gmail.com>
2022-03-28 17:27:09 +0700

    add pred-class for bmn

--- tools/class_pred.py
+++ tools/class_pred.py
@@ -1,6 +1,8 @@
 import os 
 import json 
 import torch
+import glob
+import numpy as np
 from mmaction.apis import inference_recognizer, init_recognizer
 
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
@@ -8,6 +10,7 @@ from mmaction.datasets.pipelines import Compose
 from mmcv.parallel import collate, scatter
 from mmaction.core import OutputHook
 from operator import itemgetter
+from tqdm import tqdm
 
 def inference_recognizer_multiview(model, video,outputs=None, as_tensor=True):
     cfg = model.cfg
@@ -38,6 +41,18 @@ def inference_recognizer_multiview(model, video,outputs=None, as_tensor=True):
         return top5_label, returned_features
     return top5_label
 
+def process_name(dashboard_video):
+
+    if "shboard" in dashboard_video:
+        return dashboard_video.replace("shboard", "Dashboard")
+    elif "ar_view" in dashboard_video:
+        return dashboard_video.replace("ar_view", "Dashboard")
+    elif "ghtside_window" in dashboard_video:
+        return dashboard_video.replace("ghtside_window", "Dashboard")
+    elif "ght_side_window" in dashboard_video:
+        return dashboard_video.replace("ght_side_window", "Dashboard")
+
+
 if __name__=="__main__":
     
     score_thr = 0.1
@@ -46,26 +61,29 @@ if __name__=="__main__":
     #     "ar" : "Rear",
     #     "ghtside": "Rightside"
     # }
-    video_dir = "/media/data/ai-city-2022/Track3/A2/val_video_2m"
-    json_file = json.load(open("bmn_a2.json"))
+    video_dir = "/ssd3/data/ai-city-2022/Track3/A2/val_video_2m"
+    json_file = json.load(open("bmn_a2.json", "r"))
     
     #-----loading model
-    model = init_recognizer("/home/cybercore/vinh_overal/AICity2022-Track3/configs/recognition/conNext_super_feat/aicity/convnext_vidconv_333_224_aicityA1_T1k.py", 
-                            "/home/cybercore/vinh_overal/AICity2022-Track3/current_best_e20.pth", device="cuda:0")
-    new_json_file = json_file.copy()
-    new_json_file['results'] = {}
+    model = init_recognizer("/home/ccvn/Workspace/vinh/AICity2022-Track3/configs/recognition/conNext_super_feat/aicity/convnext_vidconv_333_224_aicityA1_T1k.py", 
+                            "/home/ccvn/Workspace/vinh/AICity2022-Track3/current_best_e20.pth", device="cuda:1")
+    # new_json_file = json_file.copy()
+    # new_json_file['results'] = {}
     
+    exist_json = os.listdir("/home/ccvn/Workspace/vinh/AICity2022-Track3/tools/json_folder")
+    exist_json = [tmp.replace("json", "mp4") for tmp in exist_json]
+    os.makedirs("json_folder", exist_ok=True)
+
+    for tmp in exist_json:
+        json_file['results'].pop(tmp)
     #------------------------
-    for video_name, results in json_file['results'].items():
-        
-        
-        
-        video_name = video_name.split("_")
-        dashboard_video = video_name.copy()
-        dashboard_video[0] = "Dashboard"    
-        dashboard_video = '_'.join(dashboard_video)
-        video_name = '_'.join(video_name)
-        new_json_file['results'][video_name] = []
+    for video_name, results in tqdm(json_file['results'].items()):
+
+        new_json_file = {}
+        dashboard_video = video_name
+        dashboard_video = process_name(dashboard_video)
+        print("Video_name: ", video_name)
+        new_json_file[video_name] = []
         
         for _result in results:
             if _result['score'] < score_thr:
@@ -90,11 +108,19 @@ if __name__=="__main__":
             ffmpeg_extract_subclip(os.path.join(video_dir, rightside_video), 
                                    start_time, end_time, 
                                    targetname=rightside_video)
-            
-            pred_result = inference_recognizer_multiview(model, dashboard_video)
-            
+            try:
+                pred_result = inference_recognizer_multiview(model, dashboard_video)
+            except:
+                print("--------- Error Here------------")
+                continue
+            pred_result = [list(map(np.float64,tmp)) for tmp in pred_result]
             _result['pred'] = pred_result
-            new_json_file['results'][video_name].append(_result)
+            new_json_file[video_name].append(_result)
         
-        import ipdb; ipdb.set_trace()
+        # remove old video name 
+        filelist = glob.glob(os.path.join("./", "*.mp4"))
+        for f in filelist:
+            os.remove(f)
         
+        with open(os.path.join("json_folder", video_name.replace("mp4","json")), "w") as f:
+            json.dump(new_json_file, f)
