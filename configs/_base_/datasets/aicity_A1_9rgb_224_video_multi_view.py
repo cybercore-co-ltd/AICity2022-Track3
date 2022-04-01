c44dcdb113ca8d6e83374a4850b7cf3b18f2f0e9
vinhnguyen21 <39525953+vinhnguyen21@users.noreply.github.com>
2022-04-01 13:35:15 +0700

    Update aicity_A1_9rgb_224_video_multi_view.py

--- configs/_base_/datasets/aicity_A1_9rgb_224_video_multi_view.py
+++ configs/_base_/datasets/aicity_A1_9rgb_224_video_multi_view.py
@@ -1,7 +1,7 @@
 dataset_type = 'VideoDataset'
 # data_root = '/ssd3/data/kinetics400/'
 # data_root = '/raid/data/kinetics400/'
-data_root = '/media/data/ai-city-2022/Track3/'
+data_root = '/raid/data/ai-city-2022/Track3/'
 
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_bgr=False)
@@ -9,8 +9,8 @@ image_size = 224
 train_pipeline = [
     dict(type='CcDecordInit'),
     dict(type='RandSampleFrames', clip_len=9,
-         range=(12,15), num_clips=4),
-    dict(type='CcDecordDecode'),#, crop_drive=True),
+         range=(12, 18), num_clips=5),
+    dict(type='CcDecordDecode', crop_drive=True),
     dict(type='Resize', scale=(-1, 256)),
     dict(
         type='MultiScaleCrop',
@@ -20,22 +20,22 @@ train_pipeline = [
         max_wh_scale_gap=1,
         num_fixed_crops=13),
     dict(type='Resize', scale=(image_size, image_size), keep_ratio=False),
-    # dict(type='Imgaug', transforms='default'),
+    dict(type='Imgaug', transforms='default'),
     dict(type='Normalize', **img_norm_cfg),
-    # dict(type='RandomErasing', probability=0.1),
+    dict(type='RandomErasing', probability=0.1),
     dict(type='FormatShape', input_format='NCHW'),
     dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
     dict(type='ToTensor', keys=['imgs', 'label'])
-] 
+]
 val_pipeline = [
     dict(type='CcDecordInit'),
     dict(
         type='SampleFrames',
         clip_len=9,
-        frame_interval=15,
-        num_clips=4, 
+        frame_interval=18,
+        num_clips=5,
         test_mode=True),
-    dict(type='CcDecordDecode'),#,crop_drive=True),
+    dict(type='CcDecordDecode', crop_drive=True),
     dict(type='Resize', scale=(-1, 256)),
     dict(type='CenterCrop', crop_size=image_size),
     # dict(type='ThreeCrop', crop_size=224),
@@ -45,43 +45,27 @@ val_pipeline = [
     dict(type='ToTensor', keys=['imgs'])
 ]
 
-test_pipeline = [
-    dict(type='DecordInit'),
-    dict(
-        type='SampleFrames',
-        clip_len=9,
-        frame_interval=12,
-        num_clips=1,
-        test_mode=True),
-    dict(type='DecordDecode'),
-    dict(type='Resize', scale=(-1, 256)),
-    dict(type='CenterCrop', crop_size=image_size),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='FormatShape', input_format='NCHW'),
-    dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
-    dict(type='ToTensor', keys=['imgs'])
-]
-
 data = dict(
-    videos_per_gpu=1,
+    videos_per_gpu=4,
     workers_per_gpu=2,
     train=dict(
-        type='RepeatDataset',  # Wrapper of dataset, refer to https://github.com/open-mmlab/mmdetection/blob/master/mmdet/datasets/dataset_wrappers.py for details.
+        # Wrapper of dataset, refer to https://github.com/open-mmlab/mmdetection/blob/master/mmdet/datasets/dataset_wrappers.py for details.
+        type='RepeatDataset',
         times=1,  # Repeat times
         dataset=dict(
             type=dataset_type,
-            ann_file=data_root+'train.csv',
+            ann_file=data_root+'dashboard_train_without_bg.csv',
             data_prefix=data_root+'train_video',
             pipeline=train_pipeline
         )
     ),
     val=dict(
         type=dataset_type,
-        ann_file=data_root+'val.csv',
+        ann_file=data_root+'dashboard_val_without_bg.csv',
         data_prefix=data_root+'val_video',
         pipeline=val_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file=data_root+'val.csv',
+        ann_file=data_root+'dashboard_val_without_bg.csv',
         data_prefix=data_root+'val_video',
         pipeline=val_pipeline))
