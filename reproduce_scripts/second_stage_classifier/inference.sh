820754cd564281447fba94038720ad8848517788
vinh <ntqvinh21@gmail.com>
2022-04-15 17:21:27 +0900

    add inference

--- reproduce_scripts/second_stage_classifier/inference.sh
+++ reproduce_scripts/second_stage_classifier/inference.sh
@@ -3,8 +3,14 @@ export PYTHONPATH="$(dirname $0)/..":$PYTHONPATH
 
 CFG="configs/aicity/convnext_vidconv_333_224_aicityA1_multi_view_T1k.py"
 CKPT="http://118.69.233.170:60001/open/AICity/track3/vidconv_classifier/best_multiview_ckpt_e12.pth"
-VIDEO_DIR="/ssd3/data/ai-city-2022/Track3/raw_video/A2/"
-PROPOSAL_FILE="actionformer_mulviews_thebest_0804.json"
+VIDEO_DIR="/raid/data/ai-city-2022/Track3/raw_video/A2/" # folder which store video to test
+PROPOSAL_FILE="actionformer_mulviews_thebest_0804.json" #proposal from action-former
+OUT_DIR="ssc_json_folder"
 
+#------------ inference
 python ccaction/tools/ssc/inference_ssc.py --config $CFG --checkpoint $CKPT --proposal-thr 0.3 \
-                                        --video-dir $VIDEO_DIR --proposal $PROPOSAL_FILE
+                                        --video-dir $VIDEO_DIR --proposal $PROPOSAL_FILE --outdir $OUT_DIR
+
+
+#------------ combine all video json files for post-process
+python ccaction/tools/ssc/combine_json.py --proposal $PROPOSAL_FILE --outdir $OUT_DIR # output will be: actionformer_mulview_ssc.json
