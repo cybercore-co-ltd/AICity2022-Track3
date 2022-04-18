53c763a2bd7bf3062f8fc0b402711e75c413f31a
vinh <ntqvinh21@gmail.com>
2022-04-18 14:53:56 +0900

    ensemble infer

--- ccaction/models/heads/__init__.py
+++ ccaction/models/heads/__init__.py
@@ -2,8 +2,4 @@ from mmaction.models.heads import *
 from .vidconv_head import VidConvHead 
 from .tsp_head import TSPHead
 from .multiview_vidconv_head import MultiviewVidConvHead
-
-
-
-
-
+from .multiview_vidconv_head_s1k import MultiviewVidConvHead_S1K
