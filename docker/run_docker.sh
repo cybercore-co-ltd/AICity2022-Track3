240ab578720e78b22ad0d500518d1e0d2eb99e94
chuong98 <chuong98vt@gmail.com>
2022-04-18 20:07:40 +0900

    addada

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -3,5 +3,5 @@ DATA_DIR_FULL_VIDEO='/ssd3/data/ai-city-2022/Track3/raw_frames/full_video'
 
 docker run --rm -it --gpus all  --privileged=true --shm-size 200G\
     -v $DATA_DIR:/cctrack3/data/raw_video\
-    -v $DATA_DIR_FULL_VIDEO:/cctrack3/data/full_video\
+    -v $DATA_DIR_FULL_VIDEO:/cctrack3/data/raw_frames/full_video\
     cctrack3
