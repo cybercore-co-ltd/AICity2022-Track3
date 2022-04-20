d612e5f4daa31218fe7e361a926500acaad6d931
CCVN-Server2 <ntqvinh21@gmail.comm>
2022-04-20 19:09:23 +0900

    adjust extract_tsp_b

--- reproduce_scripts/detector/extract_tsp_b.sh
+++ reproduce_scripts/detector/extract_tsp_b.sh
@@ -3,10 +3,11 @@ INPUT_VIDEO=$2
 OUTPUT_VIDEO=$3
 mkdir B
 ls $INPUT_VIDEO > file_list_B.txt
-reproduce_scripts/detector/extract_tsp_features/extract_tsp_1.sh $CKPT file_list_B.txt $INPUT_VIDEO $OUTPUT_VIDEO &
-reproduce_scripts/detector/extract_tsp_features/extract_tsp_2.sh $CKPT file_list_B.txt $INPUT_VIDEO $OUTPUT_VIDEO &
-reproduce_scripts/detector/extract_tsp_features/extract_tsp_3.sh $CKPT file_list_B.txt $INPUT_VIDEO $OUTPUT_VIDEO &
-reproduce_scripts/detector/extract_tsp_features/extract_tsp_4.sh $CKPT file_list_B.txt $INPUT_VIDEO $OUTPUT_VIDEO
+reproduce_scripts/detector/extract_tsp_features/extract_tsp_1.sh $CKPT file_list_B.txt $INPUT_VIDEO B/ &
+reproduce_scripts/detector/extract_tsp_features/extract_tsp_2.sh $CKPT file_list_B.txt $INPUT_VIDEO B/ &
+reproduce_scripts/detector/extract_tsp_features/extract_tsp_3.sh $CKPT file_list_B.txt $INPUT_VIDEO B/ &
+reproduce_scripts/detector/extract_tsp_features/extract_tsp_4.sh $CKPT file_list_B.txt $INPUT_VIDEO B/
 
 mkdir -p tsp_features/round_b
+
 python tools/detector/concat_multiview_feats.py B/ $OUTPUT_VIDEO 
