bef0416d46358f1c48279907586c732c892e3836
Ngoc Nguyen <43519055+Ngoc-Nguyen-NIS@users.noreply.github.com>
2022-05-03 14:20:36 +0700

    Update extract_tsp_a1a2.sh

--- reproduce_scripts/detector/extract_tsp_a1a2.sh
+++ reproduce_scripts/detector/extract_tsp_a1a2.sh
@@ -7,7 +7,7 @@ reproduce_scripts/detector/extract_tsp_features/extract_tsp_2.sh $CKPT reproduce
 reproduce_scripts/detector/extract_tsp_features/extract_tsp_3.sh $CKPT reproduce_scripts/detector/extract_tsp_features/file_list_A2.txt data/raw_frames/full_video/A2 A2 &
 reproduce_scripts/detector/extract_tsp_features/extract_tsp_4.sh $CKPT reproduce_scripts/detector/extract_tsp_features/file_list_A2.txt data/raw_frames/full_video/A2 A2
 
-mkdir -p tsp_features/round1
+mkdir -p $OUTPUT_DIR
 python tools/detector/concat_multiview_feats.py A1/ $OUTPUT_DIR 
 python tools/detector/concat_multiview_feats.py A2/ $OUTPUT_DIR
 rm A1/* A2/*
